c3941df2309e238d862f34d7c7903e2d7aa2ecbe
therakeshpurohit <rakeshpurohit909@gmail.com>
2021-01-19 10:50:02 +0530

    dynamic fetch result by key

--- src/components/APIFunctionGenerator.js
+++ src/components/APIFunctionGenerator.js
@@ -1,31 +1,35 @@
 import { randomJSONAPI } from "./assets/JSON/data";
 import fs from "browserify-fs";
 export const APIFunctionGenerator = () => {
-  let apiFnTemplate = (name) => `
-return async function ${name}(randomJSONAPI) {
+  let apiFnTemplate = (randomJSONAPI) => `
+return async function ${randomJSONAPI.functionName}(randomJSONAPI) {
   const response = await fetch(randomJSONAPI.url, {
     method: randomJSONAPI.method,
-    headers: randomJSONAPI.headers
-  });
-  return response.json() + randomJSONAPI.dataKey;
+    headers: randomJSONAPI.headers,
+  })
+    .then(async (res) => {
+      let data = await res.json();
+      console.log("res data", data.${randomJSONAPI.dataKey});
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
 }`;
 
-  function createFunction(template, name) {
-    return new Function(template(name))();
+  function createFunction(template, randomJSONAPI) {
+    return new Function(template(randomJSONAPI))();
   }
 
   let functionGenerated = createFunction(
     apiFnTemplate,
-    randomJSONAPI.functionName
+    randomJSONAPI
   )(randomJSONAPI);
 
   fs.mkdir("/API", function () {
     fs.writeFile(
       "/API/randomAPI.txt",
       escape(functionGenerated),
-      function (err, data) {
-        console.log("Write : err, data", err, data);
-      }
+      function () {}
     );
   });
 };
--- src/components/Display.js
+++ src/components/Display.js
@@ -3,9 +3,9 @@ import { APIFunctionGenerator } from "./APIFunctionGenerator";
 import fs from "browserify-fs";
 function Display() {
   fs.readFile("/API/randomAPI.txt", "utf-8", async function (err, data) {
-    await console.log("read", err, unescape(data));
+    let code = await unescape(data);
   });
-  console.log("APIFunctionGenerator => ", APIFunctionGenerator());
+  console.log(APIFunctionGenerator());
 
   return <div></div>;
 }
